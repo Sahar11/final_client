b59fa07e7453a99b93636bb7f449cff7ae70a7da
satya sindhu <movva001@gmail.com>
2022-02-14 15:14:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 import Application from './components/Application';
 
 
-export default function App(){
- 
-  return (<Application/>)
-  
+export default function App() {
+
+  return (<><h1>hi</h1><Application /></>)
+
 
 }
