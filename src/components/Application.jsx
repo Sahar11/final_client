e14e83d0b972f23c793571565fddd0f39b8c47bf
Sahar11 <saharhalim151@gmail.com>
2022-02-15 22:10:12 -0500

    add patientReportPage

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Fragment } from 'react/cjs/react.production.min';
 import Patient from './Patients/Patient';
+import PatientReport from './PatientReport';
 
 export default function Application() {
   const [state, setState] = useState([]);
@@ -16,8 +17,11 @@ export default function Application() {
     .catch(error => console.log(`Error: ${error}`));
   
 }, []);
+
+
 return <Fragment>
   <h1>Hi</h1>
+  <div><PatientReport /></div>
  <div><Patient /></div>
 { 
 state.map((user, idx) => <h1 key={idx}> {user.firstname} </h1>) 
---
+++ src/components/PatientReport.jsx
@@ -0,0 +1,22 @@
+import {useEffect, useState } from "react";
+import axios from "axios";
+import "./styles/report.css"
+//import { Fragment } from "react/cjs/react.production.min";
+
+export default function PatientReport() {
+  const [report, setReport] = useState([]);
+ 
+  useEffect(() => {
+    axios.get("http://localhost:8080/report")
+    .then((res) => {
+      console.log('response', res.data);
+      setReport(res.data)
+    })
+    .catch(error => console.log(`Error: ${error}`));
+  
+}, []);
+return report.map((patient) => 
+<button className="links" key={patient.id}> {patient.report } </button>) 
+  
+  
+} 
