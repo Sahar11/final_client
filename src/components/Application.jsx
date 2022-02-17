ec4a03ad6143e7116090c423a25a5491c3523e66
Roda W <93175113+RodaW@users.noreply.github.com>
2022-02-17 12:50:58 -0500

    intial commit

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,26 +1,275 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Fragment } from 'react/cjs/react.production.min';
-import Patient from './Patients/Patient';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Fragment } from "react/cjs/react.production.min";
+import Patient from "./Patients/Patient";
+import { Calendar, Alert, Steps } from "antd";
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
+import Instructions from "./Instructions";
+
+const { Step } = Steps;
+const unavailable = [
+  "02-16-2022",
+  "02-17-2022",
+  "02-18-2022",
+  "02-21-2022",
+  "02-22-2022",
+];
+const timeOnThe25th = ["3:30", "5:00"];
+const timeSlot = [
+  ["8:00", "8:10", "8:20", "8:30", "8:40", "8:50"],
+  [
+    "9:00",
+    "9:10",
+    "9:20",
+    "9:30",
+    "9:40",
+    "9:50",
+    "10:00",
+    "10:10",
+    "10:20",
+    "10:30",
+    "10:40",
+    "10:50",
+    "11:00",
+    "11:10",
+    "11:20",
+    "11:30",
+    "11:40",
+    "11:50",
+  ],
+  [
+    "12:00",
+    "12:10",
+    "12:20",
+    "12:30",
+    "12:40",
+    "12:50",
+    "1:00",
+    "1:10",
+    "1:20",
+    "1:30",
+    "1:40",
+    "1:50",
+    "2:00",
+    "2:10",
+    "2:20",
+    "2:30",
+    "2:40",
+    "2:50",
+  ],
+  [
+    "3:00",
+    "3:10",
+    "3:20",
+    "3:30",
+    "3:40",
+    "3:50",
+    "4:00",
+    "4:10",
+    "4:20",
+    "4:30",
+    "4:40",
+    "4:50",
+    "5:00",
+    "5:10",
+    "5:20",
+    "5:30",
+    "5:40",
+    "5:50",
+    "6:00",
+  ],
+];
 
 export default function Application() {
-  const [state, setState] = useState([]);
- 
-  
+  const [value, setValue] = useState(0);
+  const [selectedValue, setSelectedValue] = useState(0);
+  const [step, setStep] = useState(0);
+  const [availableTime, setAvailableTime] = useState([]);
+
+  // let counter = 0;
+  // let timer = 3;
+  // let timeList = [];
+  // while (true) {
+  //   if (timer === 6) {
+  //     break;
+  //   }
+  //   if (counter === 60) {
+  //     timer++;
+  //     timeList.push(timer);
+  //     counter = 0;
+  //   } else {
+  //     timeList.push(timer + counter);
+  //   }
+  //   counter += 10;
+  // }
+  // console.log(timeList);
+
   useEffect(() => {
-    axios.get("http://localhost:8080")
-    .then((res) => {
-      console.log('response', res.data);
-      setState(res.data)
-    })
-    .catch(error => console.log(`Error: ${error}`));
-  
-}, []);
-return <Fragment>
-  <h1>Hi</h1>
- <div><Patient /></div>
-{ 
-state.map((user, idx) => <h1 key={idx}> {user.firstname} </h1>) 
-} 
-</Fragment> 
+    axios
+      .get("http://localhost:8080")
+      .then((res) => {
+        console.log("response", res.data);
+        //setState(res.data)
+      })
+      .catch((error) => console.log(`Error: ${error}`));
+  }, []);
+
+  const onSelect = (value) => {
+    setValue(value);
+    setSelectedValue(value);
+  };
+
+  const onPanelChange = (value) => {
+    setValue(value);
+  };
+
+  return (
+    <Fragment>
+      <div>
+        <Patient />
+      </div>
+      <div style={{ padding: "2rem" }}>
+        <Steps current={step}>
+          <Step
+            title="Select lab"
+            description="Select a lab for your appointment"
+          />
+          <Step
+            title="Patient Instructions"
+            description="Follow these instructions"
+          />
+          <Step
+            title="Choose a date"
+            description="Select a date for your appointment"
+          />
+        </Steps>
+      </div>
+      {step === 0 ? (
+        <div> <button onClick={() => setStep(1)}>I agree</button></div>
+      ) : step === 1 ? (
+        <div>
+          <Instructions />
+          <button onClick={() => setStep(2)}>I agree</button>
+        </div>
+      ) : (
+        <div style={{ padding: "2rem" }}>
+          {/* <Alert
+          message={`You selected date: ${
+            selectedValue && selectedValue.format("YYYY-MM-DD")
+          }`}
+        /> */}
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-6">
+                <Calendar
+                  fullscreen={true}
+                  value={value}
+                  onSelect={onSelect}
+                  onPanelChange={onPanelChange}
+                  disabledDate={(CellDate) =>
+                    CellDate.isBefore(new Date(), "day")
+                  }
+                  dateCellRender={(CellDate) => {
+                    let textDate = CellDate.format("MM-DD-YYYY");
+                    if (CellDate.isBefore(new Date(), "day")) {
+                      return (
+                        <div
+                          className="calendar-item"
+                          style={{
+                            backgroundColor: "#eee",
+                            width: "100%",
+                            height: "100%",
+                          }}
+                        ></div>
+                      );
+                    } else if (unavailable.includes(textDate)) {
+                      return (
+                        <div className="calendar-item">
+                          <CloseOutlined
+                            style={{ fontSize: "200%", color: "red" }}
+                          />
+                        </div>
+                      );
+                    } else if (
+                      !CellDate.isBefore(new Date(), "day") &&
+                      (CellDate.day() === 6 || CellDate.day() === 0)
+                    ) {
+                      return (
+                        <div className="calendar-item">
+                          <span style={{ color: "#555" }}>Closed</span>
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <div className="calendar-item">
+                          <CheckOutlined
+                            style={{ fontSize: "200%", color: "green" }}
+                          />
+                        </div>
+                      );
+                    }
+                  }}
+                />
+              </div>
+              <div className="col-md-6">
+                <div
+                  className="row appointment-time"
+                  style={{ maxHeight: "500px", overflow: "auto" }}
+                >
+                  <div className="col-md-3">
+                    <ul class="list-group text-center">
+                      <li class="list-group-item bg-dark text-white">
+                        Before 9:00am
+                      </li>
+                      {timeSlot[0].map((slot) => {
+                        return <li class="list-group-item">{slot}</li>;
+                      })}
+                    </ul>
+                  </div>
+                  <div className="col-md-3">
+                    <ul class="list-group text-center">
+                      <li class="list-group-item bg-dark text-white">
+                        9:00-11:55am
+                      </li>
+                      {timeSlot[1].map((slot) => {
+                        return <li class="list-group-item">{slot}</li>;
+                      })}
+                    </ul>
+                  </div>
+                  <div className="col-md-3">
+                    <ul class="list-group text-center">
+                      <li class="list-group-item bg-dark text-white">
+                        12:00-2:59pm
+                      </li>
+                      {timeSlot[2].map((slot) => {
+                        return <li class="list-group-item">{slot}</li>;
+                      })}
+                    </ul>
+                  </div>
+                  <div className="col-md-3">
+                    <ul class="list-group text-center">
+                      <li class="list-group-item bg-dark text-white">
+                        After 3:00pm
+                      </li>
+                      {timeSlot[3]
+                        .filter((slot) => {
+                          // const inSlot=timeOnThe25th.includes(slot)
+                          // if (inSlot) {
+                          //   return false
+                          // }else if ()
+                          return true;
+                        })
+                        .map((slot) => {
+                          return <li class="list-group-item">{slot}</li>;
+                        })}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </Fragment>
+  );
 }
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,36 @@
+export default function Instructions(){
+  return (<p>
+    COVID-19 nasal swab testing requirements
+
+This COVID-19 testing is for non-travel purposes.
+If you need Flight clearance testing, use the FlyClearTM locations listed here.
+
+As announced by the Ontario government on December 31, publicly-funded PCR testing will be available only for high-risk individuals who are symptomatic and/or are at risk of severe illness from COVID-19, including for the purposes of confirming a COVID-19 diagnosis to begin treatment, and workers and residents in the highest risk settings, as well as vulnerable populations. Members of the general public with mild symptoms are asked not to seek testing.
+
+Online appointments are required.
+No lab requisition form required.
+No other tests will be performed at these collection centre(s) when COVID-19 testing is being performed.
+You can get tested at select LifeLabs collection centre(s) if you meet one or more of the following criteria:
+
+Symptomatic people who fall into one of the following groups:
+Hospitalized patients
+Patients in Emergency Departments, at the discretion of the treating clinician
+Patient-facing health care workers
+Staff, residents, essential care providers, and visitors in hospitals and congregate living settings, including long-term care, retirement homes, First Nation elder care lodges, group homes, shelters, hospices, temporary foreign worker settings, and correctional institutions
+Outpatients for whom COVID-19 treatment is being considered
+Pregnant people
+People who are underhoused or homeless
+First responders, including fire, police and paramedics
+People who are from First Nation, Inuit, and Métis communities and individuals travelling into these communities for work
+Symptomatic elementary and secondary students and education staff who have received a PCR self-collection kit through their school
+People on admission/transfer to or from hospital or congregate living setting
+High-risk contacts and asymptomatic/symptomatic people in the context of confirmed or suspected outbreaks in high-risk settings, including hospitals, long-term care, retirement homes, other congregate living settings and institutions, and other settings as directed by the local public health unit
+Individuals, and one accompanying caregiver, with written prior approval for out-of-country medical services from the General Manager, OHIP
+Asymptomatic testing in hospital, long-term care, retirement homes and other congregate living settings and Institutions as per provincial guidance and/or Directives
+For children under 2 years old, please visit a COVID-19 assessment centre.
+
+As a reminder, face masks are mandatory at all locations for the health and safety of our customers, employees, and community.
+
+If you suspect you may have COVID-19, please do not visit any of the surrounding businesses or facilities before or after your swab collection at ExcelLabs.
+  </p>)
+}
