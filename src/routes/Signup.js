38fe0f135de012efbc3bde3e6d40133839936fc4
satya sindhu <movva001@gmail.com>
2022-02-17 12:03:10 -0500

    create routes for login,signup & mainpage

---
+++ src/routes/Signup.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SignupFormComp from "../components/SignupFormComp";
+
+class Signup extends Component {
+    render(login) {
+        return (
+            <div>
+                <SignupFormComp />
+            </div>
+        );
+    }
+}
+
+export default Signup;
---
+++ src/routes/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import LoginFormComp from "../components/LoginFormComp"
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <LoginFormComp history={this.props.history} setUser={this.props.setUser} />
+            </div>
+        );
+    }
+}
+
+export default Login;
