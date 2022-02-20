3f2e29c861b8066a66277b9217a247e798dcf834
Sahar11 <saharhalim151@gmail.com>
2022-02-20 18:56:53 -0500

    change Application

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,24 +3,21 @@ import axios from 'axios';
 import { Fragment } from 'react/cjs/react.production.min';
 import Patient from './Patients/Patient';
 import MainPage from './Patients/MainPage';
+import LoginForm from './Patients/LoginForm';
+import LabMain from './Patients/LabMain';
+import {Routes, Route} from 'react-router-dom';
+import SMSForm from './SMSForm';
 
 export default function Application() {
-  const [state, setState] = useState([]);
- 
-  
-  useEffect(() => {
-    axios.get("http://localhost:8080")
-    .then((res) => {
-      console.log('response', res.data);
-      setState(res.data)
-    })
-    .catch(error => console.log(`Error: ${error}`));
-  
-}, []);
 return <Fragment>
- <div><MainPage /></div>
-{/* { 
-state.map((user, idx) => <h1 key={idx}> {user.firstname} </h1>) 
-}  */}
+   <Routes>
+        <Route path="/">
+        <Route index element={<MainPage />} />
+        <Route path="Lab" element={<LabMain />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="/sms" element={<SMSForm/>} />
+        </Route>
+      </Routes>
+{/* <MainPage /> */}
 </Fragment> 
 }
