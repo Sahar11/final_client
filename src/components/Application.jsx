9f2496a000647023abf7ec680a1f9d359d423fc7
Roda W <93175113+RodaW@users.noreply.github.com>
2022-02-21 11:15:38 -0500

    application..

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,4 +1,31 @@
-import Appointment from "./Booking/Index"
-export default function Application(){
-return <Appointment/>
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Fragment } from 'react/cjs/react.production.min';
+//import Patient from './Patients/Patient';
+import LabUpload from './LabUpload';
+//import PatientReport from './PatientReport';
+import Patient from './Patients/Patient';
+import PatientReport from './PatientReport';
+import MainPage from './Patients/MainPage';
+import LoginForm from './Patients/LoginForm';
+import LabMain from './Patients/LabMain';
+import {Routes, Route} from 'react-router-dom';
+import LabLocation from './LabLocation';
+import SMSForm from './SMSForm';
+
+export default function Application() {
+return <Fragment>
+   <Routes>
+        <Route path="/">
+        <Route index element={<MainPage />} />
+        <Route path="Lab" element={<LabMain />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="/sms" element={<SMSForm/>} />
+        <Route path="/location" element={<LabLocation />} />
+        <Route path="/report" element={<PatientReport />} />
+        <Route path="/labupload" element={<LabUpload />} />
+        </Route>
+      </Routes>
+{/* <MainPage /> */}
+</Fragment> 
 }
